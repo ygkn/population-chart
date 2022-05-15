150e90936ae0fa32f0b234573cf20090acedc313
ygkn <2000ygkn0713@gmail.com>
2022-05-15 21:13:53 +0900

    add reset CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import 'ress/dist/ress.min.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1>Hello</h1>
