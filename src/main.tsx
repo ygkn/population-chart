0586807d59b80f825d87169a4050903c5cf97c47
ygkn <2000ygkn0713@gmail.com>
2022-05-15 21:10:37 +0900

    Replace Pre-installed assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from '@/App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Hello</h1>
+  </StrictMode>
 );
