262e752e25496decc6bdb05ca1d63b0a47727004
ygkn <2000ygkn0713@gmail.com>
2022-05-16 23:34:19 +0900

    Add Layout Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import type { FC } from 'react';
 
+import { Layout } from './components/Layout/Layout';
+
 export const App: FC = () => {
-  return <></>;
+  return (
+    <Layout>
+      <></>
+    </Layout>
+  );
 };
