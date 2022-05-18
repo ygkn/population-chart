daf5a9efede704c79fedc58b7a907451b2b1e6f8
ygkn <2000ygkn0713@gmail.com>
2022-05-18 13:10:26 +0900

    add getPrefectures and hooks

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,13 @@
-import { QueryClient } from 'react-query';
+import { QueryClient, UseQueryOptions } from 'react-query';
 
 export const queryClient = new QueryClient();
+
+export type QueryOptions<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends string | readonly unknown[] = string | readonly unknown[]
+> = Omit<
+  UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
+  'queryKey' | 'queryFn'
+>;
