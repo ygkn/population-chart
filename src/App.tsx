42e56f1150bed2a0739503ea44d7d26e4485876f
ygkn <2000ygkn0713@gmail.com>
2022-05-18 10:28:15 +0900

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import type { FC } from 'react';
 
 import { AppProviders } from './AppProviders';
 import { Layout } from './components/Layout/Layout';
-import { PopulationChart } from './components/PopulationChart/PopulationChart';
 
 export const App: FC = () => {
   return (
     <AppProviders>
       <Layout>
-        <PopulationChart />
+        <></>
       </Layout>
     </AppProviders>
   );
