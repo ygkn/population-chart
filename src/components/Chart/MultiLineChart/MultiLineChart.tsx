c0265cec601a688d4cdb27247655900ec90ede79
ygkn <2000ygkn0713@gmail.com>
2022-05-20 00:42:04 +0900

    fix MultiLineChart

--- src/components/Chart/MultiLineChart/MultiLineChart.tsx
+++ src/components/Chart/MultiLineChart/MultiLineChart.tsx
@@ -7,31 +7,47 @@ import {
   YAxis,
   Tooltip,
   Legend,
+  ResponsiveContainer,
+  Label,
 } from 'recharts';
 
-type Props<LineKey extends string | number, Value> = {
+type Props<LineKey extends string | number> = {
   xLabel: string;
   yLabel: string;
   lines: { key: LineKey; name: string }[];
-  data: ({ [_ in LineKey]: Value | undefined } & { lineKey: LineKey })[];
+  data: ({ [_ in LineKey]: number | undefined } & { lineKey: LineKey })[];
 };
 
-export const MultiLineChart = <K extends string | number, V>({
+export const MultiLineChart = <LineKey extends string | number>({
   xLabel,
   yLabel,
   lines,
   data,
-}: Props<K, V>): ReactElement => {
+}: Props<LineKey>): ReactElement => {
   return (
-    <LineChart width={600} height={300} data={data}>
-      {lines.map(({ key, name }) => (
-        <Line key={key} dataKey={key} name={name} connectNulls />
-      ))}
-      <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="lineKey" domain={['dataMin', 'dataMax']} label={xLabel} />
-      <YAxis domain={['dataMin', 'dataMax']} label={yLabel} />
-      <Tooltip />
-      <Legend />
-    </LineChart>
+    <ResponsiveContainer aspect={3 / 2}>
+      <LineChart
+        data={data}
+        margin={{
+          top: 24,
+          right: 24,
+          bottom: 24,
+          left: 24,
+        }}
+      >
+        {lines.map(({ key, name }) => (
+          <Line key={key} dataKey={key} name={name} connectNulls dot />
+        ))}
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="lineKey" domain={['dataMin', 'dataMax']}>
+          <Label value={xLabel} offset={-15} position="insideBottomRight" />
+        </XAxis>
+        <YAxis domain={['dataMin', 'dataMax']}>
+          <Label value={yLabel} offset={-25} position="insideTopLeft" />
+        </YAxis>
+        <Tooltip />
+        <Legend verticalAlign="top" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
