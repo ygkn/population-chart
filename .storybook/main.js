8577b01c496da07e20a82dc8c973dfd4e008e904
ygkn <2000ygkn0713@gmail.com>
2022-05-19 22:41:18 +0900

    fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,25 @@
+const path = require('path');
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+
+/**
+ * @type {import('@storybook/core-common').StorybookConfig}
+ */
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+
+    config.plugins.push(new VanillaExtractPlugin());
+
+    return config;
+  },
+};
