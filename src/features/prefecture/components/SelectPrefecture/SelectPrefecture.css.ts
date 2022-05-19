a324d5f63b89a2773b72349899a2ab90b6ffa3e3
ygkn <2000ygkn0713@gmail.com>
2022-05-19 22:46:00 +0900

    add style to SelectPrefecture

---
+++ src/features/prefecture/components/SelectPrefecture/SelectPrefecture.css.ts
@@ -0,0 +1,16 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/designToken.css';
+
+export const labels = style({
+  display: 'flex',
+  flexWrap: 'wrap',
+  rowGap: vars.space[-1],
+  columnGap: vars.space[0],
+});
+
+export const label = style({
+  display: 'flex',
+  gap: vars.space[-3],
+  alignItems: 'baseline',
+});
